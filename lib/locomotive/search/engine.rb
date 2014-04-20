04a8b8e622bcd990df341c031b42ba2d3ffe8293
did <didier.lafforgue@gmail.com>
2014-04-20 20:45:40 +0200

    assets are precompiled automatically

--- lib/locomotive/search/engine.rb
+++ lib/locomotive/search/engine.rb
@@ -1,15 +1,19 @@
 module Locomotive::Search
   class Engine < ::Rails::Engine
-    initializer "locomotive.search.concerns", before: "locomotive.content_types" do
+
+    initializer "locomotive.search.concerns", before: "locomotive.content_types" do |app|
       require "locomotive/search/content_type_reindexer"
       require "locomotive/search/concerns"
 
-      Rails.application.config.assets.precompile += ['locomotive/search_bar.css', 'locomotive/search_bar.js']
-
-      Rails.application.config.autoload_paths += %W(#{config.root}/app/services/locomotive)
+      app.config.autoload_paths += %W(#{config.root}/app/services/locomotive)
 
       Locomotive::PartialsCell.add_template(:custom_fields_form, "searchable")
       Locomotive::PartialsCell.add_template(:page_form, "searchable")
     end
+
+    initializer "locomotive.search.assets.precompile", group: :all do |app|
+      app.config.assets.precompile += ['locomotive/search_bar.css', 'locomotive/search_bar.js']
+    end
+
   end
 end
